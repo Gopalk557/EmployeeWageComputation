b45a011575823babd9329529a41aa7164a9d19d6
GOPAL THAKUR <gopalk557@gmail.com>
2022-01-14 18:13:00 +0530

    calculate emp salary

---
+++ attendance.java
@@ -0,0 +1,10 @@
+ #!/bin/bash -x
+    isPresent=1;
+      randomCheck=$((RANDOM%2));
+        if [ $isPresent -eq $randonCheck ];
+then
+        echo "Employee is present";
+else
+        echo "Empolyee is Abscent";
+fi
+empWageV1.sh (END)
