1901320f24cfa681205819c9892e86ae144b5d57
GOPAL THAKUR <gopalk557@gmail.com>
2022-01-18 10:25:02 +0530

    Find the power of 2

---
+++ Practice6.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+c=0
+i=1
+echo "Enter an integer for the base"
+read m
+echo "Enter a positive integer for the power"
+read n
+#while (( i > 0 ))
+#do
+  d=$(($m**$n))
+#done
+echo=$d
